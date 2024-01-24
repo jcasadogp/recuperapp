bb86bed0a0852c2bdb549bac86f7de16be00fe7a
jcasadogp <juls.c99@gmail.com>
2024-01-24 16:50:35 +0100

    add pages home quests and results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'results',
+    loadChildren: () => import('./pages/results/results.module').then( m => m.ResultsPageModule)
+  },
+  {
+    path: 'quests',
+    loadChildren: () => import('./pages/quests/quests.module').then( m => m.QuestsPageModule)
   }
 ];
 @NgModule({
