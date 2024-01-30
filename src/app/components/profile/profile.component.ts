eb7ad548290fd4f331eb32367ad6493d59edcf8a
jcasadogp <juls.c99@gmail.com>
2024-01-30 14:03:25 +0100

    profile and information components modals

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+})
+export class ProfileComponent  implements OnInit {
+
+  constructor(
+    private modalCntrl: ModalController,
+  ) { }
+
+  ngOnInit() {}
+
+  dismissModal(): void {
+		this.modalCntrl.dismiss().then().catch();
+  }
+}
