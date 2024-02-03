dbcd0b64b062a6d6540a52e11fb9286b9e3f4351
jcasadogp <juls.c99@gmail.com>
2024-02-03 20:38:24 +0100

    get participant info to home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { InformationComponent } from 'src/app/components/information/information.component';
 import { ProfileComponent } from 'src/app/components/profile/profile.component';
+import { Participant } from 'src/app/redcap_interfaces/participant';
+import { ParticipantService } from 'src/app/services/participant/participant.service';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +13,35 @@ import { ProfileComponent } from 'src/app/components/profile/profile.component';
 })
 export class HomePage implements OnInit {
 
+  public id: number;
+  public participant: Participant = {};
+  public currentDate: string;
+
   constructor(
     private router: Router,
     private modalCntrl: ModalController,
-  ) { }
+    private participantSrvc: ParticipantService
+  ) {
+    this.id = 118
+    this.currentDate = new Date().toLocaleDateString('es-ES', {day: '2-digit',month: 'long',year: 'numeric'})
+  }
 
   ngOnInit() {
+    this.getParticipant(null)
+  }
+
+  getParticipant(event){
+    this.participantSrvc.getParticipant(this.id).subscribe({
+      next: (data: Participant) => {
+        this.participant = data[0];
+        if (event) event.target.complete();
+      },
+      error: (err) => {
+        console.log(err)
+        if (event) event.target.complete();
+      },
+      complete: () => {}
+    })
   }
 
   async presentProfileModal(){
