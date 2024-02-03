eb249f5a586a94b4affe4903c2c9a2a5345ed27e
jcasadogp <juls.c99@gmail.com>
2024-02-03 20:38:01 +0100

    get participant info and baseline data to profile

--- src/app/services/participant/participant.service.ts
+++ src/app/services/participant/participant.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { DataService } from '../data/data.service';
-import { Participant } from 'src/app/redcap_interfaces/participant';
+import { Participant, BaselineData } from 'src/app/redcap_interfaces/participant';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,27 @@ import { Participant } from 'src/app/redcap_interfaces/participant';
 export class ParticipantService {
 
   constructor(
-    private dataSrvc: DataService
+    private dataSrvc: DataService,
+    private http: HttpClient
   ) { }
 
   getParticipant(id: number): Observable<Participant> {
     
     var record: number = id;
     var forms: string = "participantes";
-    // var fields: string[] = [""];
 
-    // return this.dataSrvc.export(record, forms, fields);
     return this.dataSrvc.export(record, forms);
+  }
+
+  getBaselineData(id: number): Observable<BaselineData>{
+    
+    var record: number = id;
+    var forms: string = "datos_basales";
+
+    return this.dataSrvc.export(record, forms);
+  }
+
+  getBasiLineDataJson(): Observable<any> {
+    return this.http.get('../../../assets/data/baseline_data.json');
   }
 }
