9768138f3b3f11a196a6d3589081c12871581353
jcasadogp <juls.c99@gmail.com>
2024-02-15 13:14:00 +0100

    change quest page open modal functions

--- src/app/pages/quests/quests.page.ts
+++ src/app/pages/quests/quests.page.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { BarthelsegComponent } from 'src/app/components/barthelseg/barthelseg.component';
 import { FacsegComponent } from 'src/app/components/facseg/facseg.component';
+import { MonitoringComponent } from 'src/app/components/monitoring/monitoring.component';
+import { NeuroQolComponent } from 'src/app/components/neuro-qol/neuro-qol.component';
 
 @Component({
   selector: 'app-quests',
@@ -19,6 +21,13 @@ export class QuestsPage implements OnInit {
   ngOnInit() {
   }
 
+  async presentMonitoringModal(){
+    const modal = await this.modalCntrl.create({
+      component: MonitoringComponent
+    });
+    return await modal.present();
+  }
+  
   async presentFacsegModal(){
     const modal = await this.modalCntrl.create({
       component: FacsegComponent
@@ -29,6 +38,13 @@ export class QuestsPage implements OnInit {
   async presentBarthelsegModal(){
     const modal = await this.modalCntrl.create({
       component: BarthelsegComponent
+    });
+    return await modal.present();
+  }
+
+  async presentNeuroQoLModal(){
+    const modal = await this.modalCntrl.create({
+      component: NeuroQolComponent
     });
     return await modal.present();
   }
