1a2eb31b472adadfdbb035b4400f36ea28527e8b
jcasadogp <juls.c99@gmail.com>
2024-04-03 01:25:47 +0200

    changes on data service for correct record format

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -10,8 +10,8 @@ import { BODYEXPORT } from 'src/app/shared/body-export';
 export class DataService {
 
   url: string = 'https://bioinfo.irycis.org/redcap/api/';
-  body_export;
-  body_import;
+  // body_export;
+  // body_import;
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -21,20 +21,24 @@ export class DataService {
   constructor(
     private http: HttpClient
   ) {
-    this.body_export = BODYEXPORT;
-    this.body_import = BODYIMPORT;
+    // this.body_export = BODYEXPORT;
+    // this.body_import = BODYIMPORT;
   }
   
 
   // ===============================================================================================
   // EXPORT DATA DE REDCAP A LA APLICACIÓN
   // ===============================================================================================
-  export(record: number, forms: string){
-    this.body_export.append("records", JSON.stringify(record));
-    this.body_export.append("forms", forms);
-    // this.body_export.append("fields", JSON.stringify(fields));
-          
-    var res = this.http.post<any>(this.url, this.body_export.toString(), this.httpOptions);
+  export(record: string, forms: string){
+    var body_export = BODYEXPORT;
+    // body_export.append("records", JSON.stringify(record));
+    body_export.append("records", record);
+    body_export.append("forms", forms);
+    
+    console.log("**", record, typeof(record))
+    console.log("**", body_export.toString())
+    
+    var res = this.http.post<any>(this.url, body_export.toString(), this.httpOptions);
     return res;
   }
   
@@ -42,8 +46,9 @@ export class DataService {
   // IMPORT DATA DE LA APLICACIÓN A REDCAP
   // ===============================================================================================
   import(data: any){
-    this.body_import.append('data', JSON.stringify(data));
-    var res = this.http.post<any>(this.url, this.body_import.toString(), this.httpOptions);
+    var body_import = BODYIMPORT;
+    body_import.append('data', JSON.stringify(data));
+    var res = this.http.post<any>(this.url, body_import.toString(), this.httpOptions);
     return res;
   }
 
