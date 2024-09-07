f7fcff82861fc67606c32edca41a40c8d4691369
jcasadogp <juls.c99@gmail.com>
2024-09-08 00:14:10 +0200

    changes in config and packages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'irycis.recuperapp',
   appName: 'recuperapp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav"
+    }
   }
 };
 
