0f85092b8725aeee8a7d00e63cf262c92a8bea7c
jcasadogp <juls.c99@gmail.com>
2024-10-01 17:51:53 +0200

    changes in storage

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
@@ -12,22 +11,34 @@ export class StorageService {
     this.init();
   }
 
-  async init() {
+  private async init() {
     // If using, define drivers here: await this.storage.defineDriver(/*...*/);
     const storage = await this.storage.create();
     this._storage = storage;
     console.log("Init Storage")
   }
+
+  public set(key: string, value: any): Promise<void> {
+    return this._storage?.set(key, value) ?? Promise.reject('Storage not initialized');
+  }
+
+  public get(key: string): Promise<any> {
+    return this._storage?.get(key) ?? Promise.resolve(null);
+  }
+
+  public remove(key: string): Promise<void> {
+    return this._storage?.remove(key) ?? Promise.reject('Storage not initialized');
+  }
  
-  public set(key: string, value: any) {
+  public set_old(key: string, value: any) {
     this._storage?.set(key, value);
   }
 
-  async get(key: string) {
+  async get_old(key: string) {
     return this._storage?.get(key) || [];
   }
 
-  async remove(key: string) {
+  async remove_old(key: string) {
     return this._storage?.remove(key) || [];
   }
 }
