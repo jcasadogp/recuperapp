385c341e0dc5d283b44d9a636d23441d6c9d9017
Julia Casado <juls.c99@gmail.com>
2025-03-11 23:58:16 +0100

    DOCUMENT the rest of the services: data, eva, participant

--- src/app/services/participant/participant.service.ts
+++ src/app/services/participant/participant.service.ts
@@ -15,24 +15,40 @@ export class ParticipantService {
     private dataSrvc: DataService,
     private http: HttpClient
   ) { }
-
+  
+  /**
+   * Retrieves participant information for a given record ID.
+   * 
+   * @param {string} id - The record ID of the participant.
+   * @returns {Observable<Participant[]>} - An observable containing the participant's data.
+   */
   getParticipant(id: string): Observable<Participant[]> {
-    
     var record: string = id;
     var forms: string = "participantes";
 
     return this.dataSrvc.export(record, forms);
   }
 
-  getBaselineData(id: string): Observable<BaselineData>{
-    
+  /**
+   * Retrieves baseline data for a given record ID.
+   * 
+   * @param {string} id - The record ID for which baseline data is requested.
+   * @returns {Observable<BaselineData>} - An observable containing the baseline data.
+   */
+  getBaselineData(id: string): Observable<BaselineData> {
     var record: string = id;
     var forms: string = "datos_basales";
 
     return this.dataSrvc.export(record, forms);
   }
 
+  /**
+   * Retrieves baseline data from a local JSON file.
+   * 
+   * @returns {Observable<any>} - An observable containing the baseline data from a JSON file.
+   */
   getBasiLineDataJson(): Observable<any> {
     return this.http.get('../../../assets/data/baseline_data.json');
   }
+
 }
